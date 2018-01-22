906cb023f843cdc53f67888015ee0c92f15d3e76
Дмитрий <34399716+DmitriyWolverine@users.noreply.github.com>
2018-01-23 01:23:18 +0300

    Add files via upload

---
+++ tst/by/htp/logic/FutureTellerTest.java
@@ -0,0 +1,102 @@
+package by.htp.logic;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import by.htp.exc.IncorectFlowerException;
+import by.htp.flower.Flower;
+
+public class FutureTellerTest {
+	
+	private FutureTeller teller;
+	
+	@Before
+	public void initFutureTeller() {
+		List <Flower> flower = new ArrayList<>();
+		try {
+			teller = new FutureTeller(flower);
+		} catch (IncorectFlowerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+	
+	@Test
+	public void testNotNullFlowersList() {
+		assertNotNull(teller.getFlowers());
+	}
+	@Test
+	public void testNotNullPredictionsMap() {
+		assertNotNull(teller.getPredictionMap());
+	}
+
+	@Test
+	public void testIfNotChamomileFlower() {
+		Flower flower = new Flower("Rose");
+		if(teller.ifChamomileFlower(flower)) {
+			fail();
+		}
+	}
+	@Test
+	public void testIfChamomileFlower() {
+		Flower flower = new Flower("Chamomile");
+		Flower camomileFromList = teller.getFlowers().get(0);
+		for(int i = 0 ; i <teller.getFlowers().size() ; ++i ) {
+			if(teller.ifChamomileFlower(teller.getFlowers().get(i))) {
+				camomileFromList = teller.getFlowers().get(i);
+				break;
+			}
+			
+		}
+		if(teller.ifChamomileFlower(flower)) {
+			assertEquals("The titles are different! ",flower.getTitle(), camomileFromList);
+		}
+	}
+	
+	@Test
+	public void testWrongFlowers() {
+		List<Flower> wrongFlowers = new ArrayList<>();
+		wrongFlowers.add(new Flower("Rose"));
+		wrongFlowers.add( new Flower("Carnation") );
+		try {
+			teller = new FutureTeller(wrongFlowers);
+		} catch (IncorectFlowerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<String> res = teller.makePredictionsForAllChamomiles();
+		if(res.size()>0) {
+			fail();
+		}
+	}
+	
+	public void testCorrectFlower() {
+		List<Flower> flowers = new ArrayList<>();
+		flowers.add(new Flower("Chamomile"));
+		try {
+			teller = new FutureTeller(flowers);
+		} catch (IncorectFlowerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<String> res = teller.makePredictionsForAllChamomiles();
+		assertTrue("Success in one flower prediction", res.size()== 1 );
+	}
+	
+	public void testSleepingTime() {
+		int topicNumber = teller.getPredictionMap().size();
+		teller.addOneTopic();
+		int newNumber = teller.getPredictionMap().size();
+		assertTrue("Add new topic!",newNumber>topicNumber);
+	}
+}
